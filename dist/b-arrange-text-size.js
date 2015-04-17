00290601f169b1f1553aed33ac55d59b29f0bc4f
Juan <beogip@gmail.com>
2015-04-17 10:27:01 -0300

    Fixed name

--- dist/b-arrange-text-size.js
+++ dist/b-arrange-text-size.js
@@ -1,11 +1,11 @@
-/* bArrangeTextSize - v0.1.0 - 2015-04-10
+/* b-arrange-text-size - v0.1.0 - 2015-04-17
 * https://github.com/bons/b-arrange-text-size
 * Copyright (c) 2015 Bons; Licensed MIT */
 
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-var MODULE_NAME = 'bArrangeTextSize';
+var MODULE_NAME = 'bons.bArrangeTextSize';
 
 var angular = require('angular');
 
